8d7436126f1697bdcf12e844faef662c5a4c44c6
Vance <vance.tw@gmail.com>
2025-04-17 02:22:20 +0800

    feat: 新增 dotenv 套件以支援環境變數管理並更新伺服器啟動邏輯

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+// Load environment variables from .env file
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+  console.log("Server is running on http://localhost:3000");
 });
